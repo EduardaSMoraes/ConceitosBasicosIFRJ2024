b901270d4a48e95b90be617ae44e613aa920dac8
EduardaSMoraes <mduda6869@gmail.com>
2024-03-14 17:32:47 -0300

    oiiii

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,27 +1,47 @@
 ﻿// cire um programa para calcular o IMC de uma pessoa e informar o status de acordo com o IMC calculado
 
-Console.WriteLine("Informe seu peso: ");
-int peso = int.Parse( Console.ReadLine());
+int opcao;
 
-Console.WriteLine("Informe sua altura: ");
-float altura = float.Parse( Console.ReadLine());
+do
+{
+    Console.WriteLine("Informe o que deseja fazer");
+    Console.WriteLine("(1) Calcular IMC");
+    Console.WriteLine("(2) Jogo de Adivinhação");
+    Console.WriteLine("(0) Encerrar");
+    opcao=Convert.ToInt32(Console.ReadLine());
+    if (opcao == 1);
+    CalcularIMC();
 
-float imc = peso / (altura * altura);
+} while (opcao != 0);
+Console.WriteLine("Obrigada por sua participação");
+Console.WriteLine("Programa Finalizado");
 
-Console.WriteLine($"Para o peso {peso} e a {altura} o imc calculado foi {imc:f1}");
+static void CalcularIMC()
+{
+    Console.WriteLine("Informe seu peso: ");
+    int peso = int.Parse(Console.ReadLine());
 
-string status = string.Empty;
-if (imc <= 16.9)
-    status = "Muito abaixo do peso";
-else if (imc <= 18.4)
-    status = "Abaixo do peso";
-else if (imc <= 24.9)
-    status = "Normal :D";
-else if (imc <= 29.9)
-    status = "Acima do peso :/";
-else if (imc <= 34.9)
-    status = "Obesidade Grau I";
-else if (imc <= 40)
-    status = "Obesidade Grau II";
-else if (imc < 40)
-    status = "Obesidade Grau III";
+    Console.WriteLine("Informe sua altura: ");
+    float altura = float.Parse(Console.ReadLine());
+
+    float imc = peso / (altura * altura);
+
+    Console.WriteLine($"Para o peso {peso} e a {altura} o imc calculado foi {imc:f1}");
+
+    string status = string.Empty;
+    if (imc <= 16.9)
+        status = "Muito abaixo do peso";
+    else if (imc <= 18.4)
+        status = "Abaixo do peso";
+    else if (imc <= 24.9)
+        status = "Normal";
+    else if (imc <= 29.9)
+        status = "Acima do peso :/";
+    else if (imc <= 34.9)
+        status = "Obesidade Grau I";
+    else if (imc <= 40)
+        status = "Obesidade Grau II";
+    else
+        status = "Obesidade Grau III";
+    Console.WriteLine($"para o IMC {imc} a classificação é {status}");
+}
